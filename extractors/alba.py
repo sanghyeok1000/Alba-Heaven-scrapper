b087e4ef60694c4fa21e8cd4805700206f455f5f
sanghyeok1000 <parksang10000@naver.com>
2022-12-25 18:47:14 +0900

    done

--- extractors/alba.py
+++ extractors/alba.py
@@ -2,6 +2,7 @@ from requests import get
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+import re
 
 def extract_alba_jobs():
     base_url = "http://www.alba.co.kr/job/list/today.asp?WsSrchKeywordWord=&hidschContainText=&hidWsearchInOut=&hidSort=&hidSortOrder=&hidSortDate=&hidListView=LIST&hidSortCnt=50&hidSortFilter=Y&hidJobKind=&hidJobKindMulti=19010000&page=1&hidSearchyn=Y&strAreaMulti=02%7C%7C%EC%A0%84%EC%B2%B4%7C%7C&schtext=&selGugun=%EC%A0%84%EC%B2%B4&lastschoolcd=&careercd=&hidCareerCD=&hidLastSchoolCD=&hidLastPayCD=&hidPayStart=&sortCnt=50"
@@ -16,18 +17,24 @@ def extract_alba_jobs():
         for post in job_posts:
             anchors = post.find_all("a")
             if len(anchors) != 0 :
+                company = post.find("span", class_="company")
+                title = post.find("span", class_="title")
+                location = post.find_all("td", class_="local")
+                for i in location:
+                    locations = re.sub('<.+?>', '', str(i), 0).strip()
+
+                pay = post.find_all("td", class_="pay")
+                for j in pay:
+                    pays = re.sub('<.+?>', '', str(j), 0).strip()
                 first_anchor = anchors[0]
                 link = first_anchor['href']
-                title = post.find_all("span", class_="title")
-                location = post.find_all("td", class_="local")
-                company = post.find_all("span", class_="company")
-                pay = post.find_all("td", class_="pay")
                 job_data = {
                     'link': f"http://www.alba.co.kr/{link}",
                     'title': title.string,
-                    'location': location.string,
+                    'location': locations,
                     'company': company.string,
-                    'pay': pay.string
+                    'pay': pays
                 }
                 results.append(job_data)
     return results
+print(extract_alba_jobs())
